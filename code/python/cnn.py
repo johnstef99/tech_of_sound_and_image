806a55ba78c212a049e885463599dcc73d93fd1e
johnstef99 <john@johnstef.com>
2022-01-05 21:43:55 +0200

    get cnn working with 0.8605 accuracy

--- code/python/cnn.py
+++ code/python/cnn.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import normalize
 
 data = pd.read_csv('../../../physionet/samples/labels.csv')
 
+# create a function that generates this file
 mfccs = pd.read_csv('./mfccs.csv')
 mfccs.set_index('index', inplace=True)
 mfccs = np.array(mfccs)
@@ -15,26 +16,37 @@ mfccs = np.array([normalize(i) for i in mfccs])
 mfccs = mfccs.reshape(len(mfccs), 13, 299, 1)
 
 x_train, x_test, y_train, y_test = train_test_split(mfccs,
-                                                    data.label.values.reshape(
+                                                    data.label.replace(-1, 0)
+                                                    .values.reshape(
                                                         len(data), 1),
                                                     test_size=0.25,
                                                     random_state=69)
 
 
 cnn = models.Sequential()
-cnn.add(layers.Conv2D(32, (2, 2), activation='tanh', input_shape=(13, 299, 1)))
-cnn.add(layers.MaxPooling2D((1, 5)))
-cnn.add(layers.Conv2D(64, (2, 2), activation='tanh'))
+cnn.add(layers.Conv2D(32, (3, 3), activation='tanh', input_shape=(13, 299, 1)))
+cnn.add(layers.MaxPooling2D((1, 10)))
+
+cnn.add(layers.Conv2D(64, (3, 3), activation='tanh'))
 cnn.add(layers.MaxPooling2D((1, 4)))
+
 cnn.add(layers.Flatten())
-cnn.add(layers.Dense(64, activation='tanh'))
-cnn.add(layers.Dense(2))
+cnn.add(layers.Dense(32, activation='tanh'))
+
+cnn.add(layers.Dense(1))
 cnn.summary()
 cnn.compile(optimizer='adam',
-            loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+            loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
             metrics=['accuracy'])
 
-cnn.fit(x_train,
-        y_train,
-        epochs=10,
-        validation_data=(x_test, y_test))
+history = cnn.fit(x_train,
+                  y_train,
+                  epochs=50,
+                  batch_size=10,
+                  validation_data=(x_test, y_test))
+
+
+test_loss, test_acc = cnn.evaluate(x_test, y_test, verbose=2)
+
+print("HERE".center(40, '='))
+print(f"Accuracy{test_acc}")
