29d386347675e1b6df77617aaf7d87ddd6d43223
johnstef99 <john@johnstef.com>
2022-01-06 17:45:46 +0200

    impove accuracy to 0.8642

--- code/python/cnn.py
+++ code/python/cnn.py
@@ -1,52 +1,72 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
+from tensorflow.keras import models
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+import logging
+import pandas as pd
+import tensorflow as tf
+from mfcc import generate_or_load_mfccs
+from matplotlib import pyplot as plt
 
-data = pd.read_csv('../../../physionet/samples/labels.csv')
-
-# create a function that generates this file
-mfccs = pd.read_csv('./mfccs.csv')
-mfccs.set_index('index', inplace=True)
-mfccs = np.array(mfccs)
-mfccs = mfccs.reshape(len(mfccs), 13, 299)
-mfccs = np.array([normalize(i) for i in mfccs])
-mfccs = mfccs.reshape(len(mfccs), 13, 299, 1)
-
-x_train, x_test, y_train, y_test = train_test_split(mfccs,
-                                                    data.label.replace(-1, 0)
-                                                    .values.reshape(
-                                                        len(data), 1),
-                                                    test_size=0.25,
-                                                    random_state=69)
+# config logger
+logging.basicConfig(level=logging.INFO,
+                    format="---------------------\n%(levelname)s %(asctime)s:\n%(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+log = logging.getLogger(__name__)
 
 
-cnn = models.Sequential()
-cnn.add(layers.Conv2D(32, (3, 3), activation='tanh', input_shape=(13, 299, 1)))
-cnn.add(layers.MaxPooling2D((1, 10)))
-
-cnn.add(layers.Conv2D(64, (3, 3), activation='tanh'))
-cnn.add(layers.MaxPooling2D((1, 4)))
-
-cnn.add(layers.Flatten())
-cnn.add(layers.Dense(32, activation='tanh'))
-
-cnn.add(layers.Dense(1))
-cnn.summary()
-cnn.compile(optimizer='adam',
-            loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-            metrics=['accuracy'])
-
-history = cnn.fit(x_train,
-                  y_train,
-                  epochs=50,
-                  batch_size=10,
-                  validation_data=(x_test, y_test))
+def get_labels():
+    return pd.read_csv('../../../physionet/samples/labels.csv')
 
 
-test_loss, test_acc = cnn.evaluate(x_test, y_test, verbose=2)
+def plot_history(history):
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.7, 1])
+    plt.legend(loc='lower right')
+    plt.show()
 
-print("HERE".center(40, '='))
-print(f"Accuracy{test_acc}")
+
+def main():
+    labels = get_labels()
+    mfccs = generate_or_load_mfccs(labels.audio.values)
+    labels = labels.label.replace(-1, 0).values.reshape(len(labels), 1)
+    x_train, x_test, y_train, y_test = train_test_split(mfccs,
+                                                        labels,
+                                                        test_size=0.25,
+                                                        random_state=69)
+
+    log.info('Creating model')
+    cnn = models.Sequential()
+    cnn.add(Conv2D(32, (3, 3), activation='tanh', input_shape=(13, 299, 1)))
+    cnn.add(MaxPooling2D((1, 10)))
+
+    cnn.add(Conv2D(64, (3, 3), activation='tanh'))
+    cnn.add(MaxPooling2D((1, 4)))
+
+    cnn.add(Flatten())
+    cnn.add(Dense(32, activation='tanh'))
+
+    cnn.add(Dense(1))
+    cnn.compile(optimizer='adam',
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+    log.info('Start training')
+
+    history = cnn.fit(x_train,
+                      y_train,
+                      epochs=50,
+                      batch_size=int(len(x_train)/300),
+                      validation_data=(x_test, y_test))
+
+    plot_history(history)
+
+    test_loss, test_acc = cnn.evaluate(x_test, y_test)
+
+    log.info(f"Accuracy: {test_acc}")
+
+
+if __name__ == '__main__':
+    main()
--- code/python/mfcc.py
+++ code/python/mfcc.py
@@ -1,8 +1,13 @@
+from sklearn.preprocessing import normalize
 from librosa import load as load_wav
 from matplotlib import pyplot as plt
 from python_speech_features import mfcc
 import numpy as np
 import pandas as pd
+from os.path import exists
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def read_audio(name: str):
@@ -85,4 +90,30 @@ def generate_mfcc(name: str, savefig: bool = False, show: bool = False):
             plt.figure(figsize=(300, 13))
             plt.imshow(mfccs)
             plt.show()
+    return mfccs
+
+
+def generate_or_load_mfccs(samples: np.ndarray = None):
+    if(exists('./mfccs.csv')):
+        mfccs = pd.read_csv('./mfccs.csv', index_col='audio')
+    else:
+        if(samples is None):
+            log.error(
+                '''mfccs.csv file doesn\'n exists and you havent passed an
+                array of samples to generate it''')
+            exit(1)
+
+        log.info(f'Start generating MFCCs for {len(samples)} samples')
+        data = np.array(list(map(generate_mfcc, samples)))
+        log.info('Done generatinge MFCCs')
+
+        data = data.reshape(len(data), 13*299)
+        mfccs = pd.DataFrame(data)
+        mfccs.set_index(samples, inplace=True)
+        mfccs.to_csv('./mfccs.csv', index_label='audio')
+
+    mfccs = np.array(mfccs)
+    mfccs = mfccs.reshape(len(mfccs), 13, 299)
+    mfccs = np.array([normalize(i) for i in mfccs])
+    mfccs = mfccs.reshape(len(mfccs), 13, 299, 1)
     return mfccs
